dd49ad51e186f1927d53d7e33d47ebc07232f1b0
sbkim21.kr <sbkim21@gmail.com>
2025-09-15 21:15:44 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+# print(cv2.__version__)
+
+image=cv2.imread('/home/sbkim21/Documents/python/OpenCVpractice/download/lena.png')
+
+if image is None:
+    print("Error: Could not read image.")
+else:
+    cv2.imwrite('output.png',image)
+    print("Image saved as output.png")
+
+
+
+
